d7cbc07aa7a034fef357c3053b947b9669d369f6
Walisson Aguirra <walissonaguirra@icloud.com>
2024-07-31 14:44:05 -0300

    feat: Filtra dados enviados pelo WebHook Woocommerce

--- wp-checkout.php
+++ wp-checkout.php
@@ -36,3 +36,76 @@ add_filter( 'wc_add_to_cart_message_html', '__return_false' );
 add_filter( 'woocommerce_add_to_cart_redirect', function (){
     return wc_get_checkout_url();
 });
+
+/**
+ * Desativar woocommerce_webhook_deliver_async no WooCommerce
+ * para que os webhooks sejão enviados imediatamente
+ */
+add_filter('woocommerce_webhook_deliver_async', '__return_false' );
+
+/**
+ * Filtra dados enviados pelo WebHook Woocommerce
+ * 
+ * @filter woocommerce_webhook_payload
+ * @priority 10
+ * @arguments 4
+ * 
+ * @param array $payload
+ * @param \WC_Order|\WC_Product|WP_User $resource
+ * @param int $resource_id
+ * @param string $webhook_id
+ * @return array New payload
+ */
+add_filter('woocommerce_webhook_payload', function($payload, $resource) {
+
+	/**
+	 * Pega o valor de meta campo no pedido
+	 *
+	 * @param  array   $meta_data  Meta campos
+	 * @param  string  $key        Key do meta campo
+	 */
+    function get_meta_data(array $meta_data, string $key) 
+    {
+    	$data = array_filter($meta_data, function ($item) use ($key) {
+    		return $item['key'] == $key;
+    	});
+
+    	return $data[array_key_first($data)]['value'] ?? null;
+    }
+
+	if ($resource == 'order') {
+	    return [
+	    	// Informações pessoais
+		    'persontype' => $payload['billing']['persontype'],
+		    'birthdate'  => get_meta_data($payload['meta_data'], '_billing_birthdate'),
+		    'cpf'        => $payload['billing']['cpf'],
+		    'cnpj'       => $payload['billing']['cnpj'],
+		    'name'       => $payload['billing']['first_name'] . ' ' . $payload['billing']['last_name'],
+		    'company'    => $payload['billing']['company'],
+		    'email'      => $payload['billing']['email'],
+		    'phone'      => $payload['billing']['phone'],
+		    'password'   => get_meta_data($payload['meta_data'], '_billing_user_password'),
+
+		    // Infomações de endereço
+		    'address'    => [
+			    'zipcode'    => $payload['billing']['postcode'],
+		    	'street'     => $payload['billing']['address_1'],
+			    'number'     => $payload['billing']['number'],
+			    'complement' => $payload['billing']['address_2'],
+			    'state'      => $payload['billing']['state'],
+			    'city'       => $payload['billing']['city']
+		    ],
+
+		    // Informações sobre a compra
+		    'woocommerce_order' => [
+		    	'id'             => $payload['line_items'][0]['product_id'],
+		    	'title'          => $payload['line_items'][0]['name'],
+		    	'price'          => $payload['line_items'][0]['price'],
+		    	'payment_status' => $payload['status']
+		    ]
+		];
+    }
+
+    return $payload;
+
+}, 10, 4);
