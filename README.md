56f4fa66735e23ebf40880b17e86e0ec8feae8e9
Walisson Aguirra <walissonaguirra@icloud.com>
2024-08-03 15:42:11 -0300

    REFACTOR

--- README.md
+++ README.md
@@ -1,51 +1,84 @@
 ## WP Chekout
-Este plugin faz modificações no fluxo de checkout do woocommerce e modifica a resposta do envida pelo WooCommerce WebHook
+Este plugin faz modificações no fluxo de checkout do woocommerce e se integra com a API epostal
 
 ### Ações do plugin
 - Limpar o carrinho antes de adicionar um novo item
 - Desabilita alerta: Produto adicionado ao carrinho
 - Redireciona direto para página de checkout
-- Desativar woocommerce_webhook_deliver_async no WooCommerce para que os webhooks sejão enviados imediatamente
-- Filtra dados enviados pelo WebHook Woocommerce
+- Impedir que um webhook mau sucedido seja desativado
+- Filtra dados enviados pelo WebHook Woocommerce durante integração
+- Reireciona página 'Shop' para pagina inicial do ePostal
+- Desabilita webHook async (comentado por padrão)
+- Adicionar código JavaScript na página de Checkout WooCommerce
+- Adicionar botão com magic-link para login automatico na epostal dash
+- Integração: Cadastra usuário na eposta
+- Integração: Confirma status de pagamento na eposta
 
-### WebHook: Order.Created
-Payloand envia pelo webhook
+### WebHook: woocommerce_checkout_process
+Payloand enviada pelo webhook para cadastro de usuário
 ```json
 {
-  "id": 47,
-  "name": "Walisson Aguirra",
-  "email": "walissonaguirra@icloud.com",
-  "password": "Mudar123",
-  "phone": "(11) 99999-9999",
-  "document": "88899977766",
-  "birthdate": "04/01/2000",
-  "address": "Avenida Castelo Branco",
-  "number": "0000",
+  "name": "Harry Potter",
+  "email": "harrypotter@epostal.com",
+  "password": "Trasgu_00",
+  "phone": "(00) 00000-0000",
+  "document": "337.116.950-27",
+  "birthdate": "01/01/2000",
+
+  "address": "Rua dos Alfeneiros",
+  "number": "4",
   "complement": "",
-  "district": "Santa Lucia",
-  "city": "Campinas",
-  "state": "SP",
-  "zipcode": "88888-888",
-  "cnpj": "02992059000139",
-  "company_name": "Joao Macaxeira",
-  "company_zipcode": "88888-888",
-  "company_address": "Avenida Castelo Branco",
-  "company_city": "Campinas",
-  "company_state": "SP",
-  "company_number": "0000",
-  "company_complement": "",
-  "plan": "Empresa",
-  "billingType": "BOLETO",
-  "value": 397,
-  "paymentId": "pay_06e9va0mm9o6i9il"
+  "district": "Little Whinging",
+  "city": "Florianópolis",
+  "state": "SC",
+  "country": "BR",
+  "zipcode": "88060-225",
+
+  "cnpj": "44.020.614/0001-00",
+  "company_name": "Harry Potter LTDA",
+  "company_zipcode": "88060-225",
+  "company_address": "Rua dos Alfeneiros",
+  "company_city": "Florianópolis",
+  "company_state": "SC",
+  "company_number": "4",
+  "company_complement": ""
+}
+```
+Os campos são válidados no backend da API ePostal e as mensagem de erro são exibidar como alerta na tela de checkout
+
+### Resposta WebHook: woocommerce_checkout_process
+Após o postagem dos dados do pedido feita pelo WebHook: woocommerce_checkout_process, ele espera receber os seguinte dados como respota:
+```json
+{
+  "data": {
+    "magic-link": "https://login.epostal.com.br/[hash_unica_para_login]"
+  }
+}
+```
+Este magic-link será usando na página: Detalhes do pedido, para que o usuário possa fazer login na ePosta Dash
+
+### WebHook: woocommerce_webhook_http_args
+Payload para obter o Bearer Token 
+```json
+{
+  "email": "harrypotter@epostal.com",
+  "password": "Trasgu_00",
+  "fcm_token": "wp_token",
+  "device_name": "Wp"
 }
 ```
 
-### Resposta WebHook: Order.Updated
-Após o postagem dos dados do pedido feita pelo webhook woocommerce, ele espera receber os seguinte dados como respota:
+### WebHook: woocommerce_webhook_payload
+Payload para atualiza o status de pagamento for confirmado
 ```json
 {
-  "id": 47,
-  "magic-link": "https://login.epostal.com.br/[hash_unica_para_login]"
+  "email": "harrypotter@epostal.com",
+  "password": "Trasgu_00", // Nessecario para obter o Bearer Token
+  "plan": "Empresa",
+  "billingType": "BOLETO", // ASAAS
+  "status": "processing", 
+  "value": "397.00", // String
+  "paymentId": 47 // ASAAS ID
 }
 ```
+Quando o GetWay de pagamento responde com a confirmação o WooCommerce atualiza o status do pedido para `processing`, (Significa pagamento recebido)
--- wp-checkout.php
+++ wp-checkout.php
@@ -4,7 +4,7 @@
  * Plugin Name:       WP Checkout
  * Plugin URI:        
  * Description:       Custumização para o fluxo do woocommerce checkout
- * Version:           0.0.1
+ * Version:           0.3.0
  * Requires at least: 6.6
  * Requires PHP:      7.4
  * Author:            Walisson Aguirra
@@ -17,134 +17,16 @@
  * Requires Plugins:  woocommerce
  */
 
-/**
- * Limpar o carrinho antes de adicionar um novo item
- */
-add_filter( 'woocommerce_add_to_cart_validation', function ($passed) {
-	WC()->cart->empty_cart();
-    return $passed;
-});
+if (!defined('ABSPATH')) {
+	exit;
+}
 
-/**
- * Desabilita alerta: Produto adicionado ao carrinho
- */
-add_filter( 'wc_add_to_cart_message_html', '__return_false' );
+require_once 'class-setup.php';
+require_once 'class-code-snippet.php';
+require_once 'class-checkout-process.php';
+require_once 'class-order-process.php';
 
-/**
- * Redireciona direto para página de checkout
- */
-add_filter( 'woocommerce_add_to_cart_redirect', function (){
-    return wc_get_checkout_url();
-});
-
-/**
- * Desativar woocommerce_webhook_deliver_async no WooCommerce
- * para que os webhooks sejão enviados imediatamente
- */
-add_filter('woocommerce_webhook_deliver_async', '__return_false' );
-
-/**
- * Filtra dados enviados pelo WebHook Woocommerce
- * 
- * @filter woocommerce_webhook_payload
- * @priority 10
- * @arguments 4
- * 
- * @param array $payload
- * @param \WC_Order|\WC_Product|WP_User $resource
- * @param int $resource_id
- * @param string $webhook_id
- * @return array New payload
- */
-add_filter('woocommerce_webhook_payload', function($payload, $resource) {
-
-	/**
-	 * Pega o valor de meta campo no pedido
-	 *
-	 * @param  array   $meta_data  Meta campos
-	 * @param  string  $key        Key do meta campo
-	 */
-    function get_meta_data(array $meta_data, string $key) 
-    {
-    	$data = array_filter($meta_data, function ($item) use ($key) {
-    		return $item['key'] == $key;
-    	});
-
-    	return $data[array_key_first($data)]['value'] ?? null;
-    }
-
-	if ($resource == 'order') {
-
-		$asaas = json_decode(get_meta_data($payload['meta_data'], '__ASAAS_ORDER'));
-
-	    return [
-	    	// WP Post ID
-	    	'id' => $payload['id'],
-
-	    	// Informações pessoa fisica
-		    'name'     	=> $payload['billing']['first_name'] . ' ' . $payload['billing']['last_name'],
-		    'email'    	=> $payload['billing']['email'],
-		    'password' 	=> get_meta_data($payload['meta_data'], '_billing_user_password'),
-		    'phone'    	=> $payload['billing']['phone'],
-		    'document'  => $payload['billing']['cpf'],
-		    'birthdate' => get_meta_data($payload['meta_data'], '_billing_birthdate'),
-
-		    // Infomações de endereço
-	    	'address'    => $payload['billing']['address_1'],
-			'number'     => $payload['billing']['number'],
-		    'complement' => $payload['billing']['address_2'],
-		    'district'   => $payload['billing']['neighborhood'],
-		    'city'       => $payload['billing']['city'],
-		    'state'      => $payload['billing']['state'],
-			'zipcode'    => $payload['billing']['postcode'],
-
-		    // Informações pessoa juridica
-		    'cnpj'               => $payload['billing']['cnpj'],
-		    "company_name"       => $payload['billing']['company'],
-		    "company_zipcode"    => $payload['billing']['postcode'],
-		    "company_address"    => $payload['billing']['address_1'],
-		    "company_city"       => $payload['billing']['city'],
-		    "company_state"      => $payload['billing']['state'],
-		    "company_number"     => $payload['billing']['number'],
-		    "company_complement" => $payload['billing']['address_2'],
-
-		    // Informações de pagamento
-		    "email"       => $payload['billing']['email'],
-		    "plan"        => "Empresa",
-		    "billingType" => $asaas->billingType,
-		    "value"       => $asaas->value,
-		    "paymentId"   => $asaas->id
-		];
-    }
-
-    return $payload;
-
-}, 10, 4);
-
-/**
- * Salva á resposta do WebHook em meta campo do pedido
- */
-add_action('woocommerce_webhook_delivery', function($http_args, $response, $duration, $arg, $webhook_id) {
-
-	if ($response['response']['code'] == 200 && isset($response['body']['magic-link'])) {
-
-		update_post_meta(
-			$response['body']['id'],
-			'magic-link',
-			$response['body']['magic-link']
-		);
-
-	}
-
-}, 1, 5);
-
-/**
- * Adicionar botão com magic-link para login na plataform
- */
-add_action('woocommerce_order_details_after_order_table', function ($order) {
-
-	echo <<<HTML
-	<a class="button alt" target="_black" href="{$order->get_meta('magic-link')}">Acessa á plataforma</a>
-	HTML;
-
-}, 10);
+new WPC_Setup();
+new WPC_Code_Snippet();
+new WPC_Checkout_Process();
+new WPC_Order_Process();
